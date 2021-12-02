3453da277fda70a423882afd34423b9ce94bea0e
Snowtype <nephritee10@gmail.com>
2021-12-02 23:34:18 +0900

    "Sorting Algorithm Practice - Web Source"

--- personalPractice/MergeSortPractice.java
+++ personalPractice/MergeSortPractice.java
@@ -3,5 +3,26 @@ package personalPractice;
 public class MergeSortPractice {
     public static void main(String[] args) {
 
+
+
+
+
+
+        int[] arr = {10,7,1,5,3,9,8,4,6,2};
+
+        int i = 0;
+        int j = arr.length / 2; // middle + 1
+        int k = i; // index
+
+        while(i <= j-1 && j <= arr.length - 1) {
+            if (j>)
+        }
+
+        System.out.println(7/2);
+    }
+
+    public static void merge(int[] arr, int start, int end) {
+        int mid = (start + end) / 2;
+        int
     }
 }
