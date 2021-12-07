d00f699b48485e9ea84b24e11246617bc15c4d66
Snowtype <nephritee10@gmail.com>
2021-12-07 21:39:51 +0900

    "Sorting Algorithm Practice - Web Source"

--- personalPractice/MergeSortPractice.java
+++ personalPractice/MergeSortPractice.java
@@ -1,22 +1,23 @@
 package personalPractice;
 
 public class MergeSortPractice {
+
+    //field : a variable inside a class,
+    private static int[] arr = {10, 7, 1, 5, 3, 9, 8, 4, 6, 2};
+    private static int[] sorted = new int[10];
+
     public static void main(String[] args) {
-
-
-
-
-        int[] arr = {10,7,1,5,3,9,8,4,6,2};
 
         int i = 0;
         int j = arr.length / 2; // middle + 1
         int k = i; // index
 
-        while(i <= j-1 && j <= arr.length - 1) {
-            if (j>)
+
+        while (i <= j - 1 && j <= arr.length - 1) {
+            if (j >)
         }
 
-        System.out.println(7/2);
+        System.out.println(7 / 2);
     }
 
     public static void mergeSort(int) {
@@ -29,9 +30,9 @@ public class MergeSortPractice {
         int right = mid + 1;
         int idx = start;
 
-        while (idx <= mid && right <= end) {
+        while (left <= mid && right <= end) {
             if (arr[left] <= arr[right]) {
-                sorted[idx] = a[left];
+                sorted[idx] = arr[left];
                 idx
             }
         }
