3de4368243543c1e9d54c65dfe7e449abf233076
Snowtype <nephritee10@gmail.com>
2021-12-09 00:08:35 +0900

    "Sorting Algorithm Practice - Web Source"

--- personalPractice/MergeSortPractice.java
+++ personalPractice/MergeSortPractice.java
@@ -3,15 +3,19 @@ package personalPractice;
 public class MergeSortPractice {
 
     //field : a variable inside a class,
-    private static int[] arr = {10, 7, 1, 5, 3, 9, 8, 4, 6, 2};
+//    private static int[] arr = {10, 7, 1, 5, 3, 9, 8, 4, 6, 2};
 
-
+    private static int[] arr = {8, 7, 6, 4};
 
     public static void main(String[] args) {
 
         StringBuilder sb = new StringBuilder();
 
         int[] tmp = new int[arr.length];
+        for (int i = 0; i <= arr.length-1; i++) {
+            tmp[i] = arr[i];
+        }
+
         mergeSort(arr, tmp, 0, arr.length - 1);
 
         for (int data : tmp) {
@@ -32,9 +36,7 @@ public class MergeSortPractice {
     }
 
     public static void merge(int[] arr, int[] tmp, int start, int end) {
-        for (int i = start; i <= end; i++) {
-            tmp[i] = arr[i];
-        }
+
 
         int mid = (start + end) / 2;
         int left = start;
@@ -43,17 +45,17 @@ public class MergeSortPractice {
 
         while (left <= mid && right <= end) {
             if (arr[left] <= arr[right]) {
-                arr[idx] = tmp[left];
+                tmp[idx] = arr[left];
                 left++;
             } else {
-                arr[idx] = tmp[right];
+                tmp[idx] = arr[right];
                 right++;
             }
             idx++;
         }
 
-        if(left <= mid) for (int i = left; i <= mid; i++) arr[idx++] = tmp[i];
-        else for(int i = right; i <= end; i++) arr[idx++] = tmp[i];
+        if(left <= mid) for (int i = left; i <= mid; i++) tmp[idx++] = arr[i];
+        else for(int i = right; i <= end; i++) tmp[idx++] = arr[i];
 //        for (int i = 0; i < mid - left; i++) {
 //            arr[idx + i] = tmp[left + i];
 //        }
