ee739f8da7afd7e9dc3770827956e9ffb740b9ac
Snowtype <nephritee10@gmail.com>
2021-12-08 23:17:02 +0900

    "Sorting Algorithm Practice - Web Source"

--- personalPractice/MergeSortPractice.java
+++ personalPractice/MergeSortPractice.java
@@ -4,31 +4,38 @@ public class MergeSortPractice {
 
     //field : a variable inside a class,
     private static int[] arr = {10, 7, 1, 5, 3, 9, 8, 4, 6, 2};
-    private static int[] sorted = new int[10];
+
+
 
     public static void main(String[] args) {
 
         StringBuilder sb = new StringBuilder();
 
+        int[] tmp = new int[arr.length];
+        mergeSort(arr, tmp, 0, arr.length - 1);
 
-        mergeSort(arr,0,arr.length-1);
-
-        for (int data : sorted) {
+        for (int data : tmp) {
             sb.append(data + ", ");
         }
-
+        sb.deleteCharAt(sb.lastIndexOf(","));
+        System.out.println(sb);
     }
 
-    public static void mergeSort(int[] arr, int start, int end) {
-        if(start < end) {
+    public static void mergeSort(int[] arr, int[] tmp, int start, int end) {
+
+        if (start < end) {
             int mid = (start + end) / 2;
-            mergeSort(arr, start, mid);
-            mergeSort(arr,mid+1, end);
-            merge(arr,start,end);
+            mergeSort(arr, tmp,start, mid);
+            mergeSort(arr, tmp, mid + 1, end);
+            merge(arr, tmp, start, end);
         }
     }
 
-    public static void merge(int[] arr, int start, int end) {
+    public static void merge(int[] arr, int[] tmp, int start, int end) {
+        for (int i = start; i <= end; i++) {
+            tmp[i] = arr[i];
+        }
+
         int mid = (start + end) / 2;
         int left = start;
         int right = mid + 1;
@@ -36,20 +43,20 @@ public class MergeSortPractice {
 
         while (left <= mid && right <= end) {
             if (arr[left] <= arr[right]) {
-                sorted[idx] = arr[left];
+                arr[idx] = tmp[left];
                 left++;
             } else {
-                sorted[idx] = arr[right];
+                arr[idx] = tmp[right];
                 right++;
             }
             idx++;
         }
 
-        for (int i = 0; i < mid - left; i++) {
-            sorted[idx + i] = arr[left + i];
-        }
-
-
+        if(left <= mid) for (int i = left; i <= mid; i++) arr[idx++] = tmp[i];
+        else for(int i = right; i <= end; i++) arr[idx++] = tmp[i];
+//        for (int i = 0; i < mid - left; i++) {
+//            arr[idx + i] = tmp[left + i];
+//        }
 
     }
 }
