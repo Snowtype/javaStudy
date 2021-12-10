0e32649024104ea44c794c05d0e5e4ca114c450d
Snowtype <nephritee10@gmail.com>
2021-12-11 02:51:53 +0900

    "Sorting Algorithm Practice - Web Source"

--- personalPractice/MergeSortPractice.java
+++ personalPractice/MergeSortPractice.java
@@ -16,9 +16,9 @@ public class MergeSortPractice {
         int[] tmp = new int[arr.length];
 
 
-        mergeSort(arr, tmp, 0, arr.length - 1);
-
-        for (int data : tmp) {
+        //mergeSort(arr, tmp, 0, arr.length - 1);
+        mergeSort2(arr, tmp2,  0, arr.length - 1);
+        for (int data : tmp2) {
             sb.append(data + ", ");
         }
         sb.deleteCharAt(sb.lastIndexOf(","));
@@ -36,7 +36,6 @@ public class MergeSortPractice {
     }
 
     public static void merge(int[] arr, int[] tmp, int start, int end) {
-
 
         int mid = (start + end) / 2;
         int left = start;
@@ -59,18 +58,20 @@ public class MergeSortPractice {
 //        for (int i = 0; i < mid - left; i++) {
 //            arr[idx + i] = tmp[left + i];
 //        }
-        for(int i=start; i<=end; i++) arr[i] = tmp[i];
 
+        for(int i=start; i<=end; i++) arr[i] = tmp[i];
     }
 
 
-
+//-----------------------2------------------------
 
     public static void mergeSort2(int[] arr, int[] tmp2, int start, int end) {
-        int mid = (start + end) / 2;
-        mergeSort2(arr, tmp2, start, mid);
-        mergeSort2(arr, tmp2, mid + 1, end);
-        merge2(arr, start, end);
+        if(start<end) {
+            int mid = (start + end) / 2;
+            mergeSort2(arr, tmp2, start, mid);
+            mergeSort2(arr,  tmp2,mid + 1, end);
+            merge2(arr, start, end);
+        }
     }
 
 
@@ -82,9 +83,16 @@ public class MergeSortPractice {
 
         while (left <= mid && right <= end) {
             if(arr[left] < arr[right]){
-                tmp2[idx++]
+                tmp2[idx++] = arr[left++];
+            } else {
+                tmp2[idx++] = arr[right++];
             }
         }
+
+        if(left <= mid) for(int i=left; i<=mid; i++) tmp2[idx++] = arr[i];
+        else if (right <= end) for (int i = right; i <= end; i++) tmp2[idx++] = arr[i];
+
+        for(int i=start; i<=end; i++) arr[i] = tmp2[i];
 
     }
 }
